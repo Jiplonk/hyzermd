6d09a229861e6bdc2e2cd04f3c624905f218e386
KazuKi <100437797+Jiplonk@users.noreply.github.com>
2022-04-09 18:59:37 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -8,13 +8,13 @@ const q = {
 	},
 	"message": {
 		"contactMessage": {
-			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;Elyas Gantwng;;;FN:Elyas Ganteng\nORG:Elyas Ganteng;\nTEL;type=CELL;type=VOICE;waid=6285643354073:+62 856-4335-4073\nEND:VCARD"
+			"vcard": "BEGIN:VCARD\nVERSION:3.0\nN:;KazuKiGans;;;FN:KazuKiGans\nORG:KazuKiGans;\nTEL;type=CELL;type=VOICE;waid=6283113989278:+6283113989278\nEND:VCARD"
 		}
 	},
 	"participant": "0@s.whatsapp.net"
 }
 
-conn.sendContact(m.chat, '6287892711054', 'Hyzerr', m)
+conn.sendContact(m.chat, '6283113989278', 'KazuKiMd', m)
 m.reply(m.chat,'*Tuh nomor owner ku tersayang*\n*Chat jika PENTING*', q)
 }
 handler.help = ['owner', 'creator']
